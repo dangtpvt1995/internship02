6926e4a6f483c496daeb4064c46321afe0b5065f
QUEPHUONGPC\Admin <phuongchung0402@gmail.com>
2018-08-09 17:20:26 +0700

    Trang home
    Trang home

--- SourvceCode_ManagerStaff/src/app/all-common/header/header.component.ts
+++ SourvceCode_ManagerStaff/src/app/all-common/header/header.component.ts
@@ -11,5 +11,9 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  imageUrl ='http://itsol.vn/wp-content/uploads/2017/01/logo_1.png';
+  imageFace='./../../../assets/hinh/iconfacebook_32x32.png';
+  imageTwitter="./../../../assets/hinh/tiwtter_32x32.png";
+  imageInsta="./../../../assets/hinh/instagram_32x32.png";
+  
 }
