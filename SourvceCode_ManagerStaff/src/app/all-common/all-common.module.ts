c08c365b8145cb810f626f9962f92c4ae66a13be
Nguyen Van Dang <dangtpvt220695@gmail.com>
2018-08-10 10:00:57 +0700

    Thêm trang home

--- SourvceCode_ManagerStaff/src/app/all-common/all-common.module.ts
+++ SourvceCode_ManagerStaff/src/app/all-common/all-common.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports:[HeaderComponent,FooterComponent],
-  declarations: [ HeaderComponent, FooterComponent],
+  exports:[],
+  declarations: [],
 })
 export class AllCommonModule { }
--- SourvceCode_ManagerStaff/src/app/app.component.ts
+++ SourvceCode_ManagerStaff/src/app/app.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'Company';
-  
+  imgLogoITsol ='./../assets/hinh/logo_2_itsol.png';
+  imgFacebook='./../assets/hinh/iconfacebook_32x32.png';
+  imgTwitter="./../assets/hinh/tiwtter_32x32.png";
+  imgInsta="./../assets/hinh/instagram_32x32.png"; 
 }
